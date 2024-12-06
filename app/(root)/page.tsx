55269a279ae1a541bd42319e5773cecf1ff4926d
Bruno Ramos Lemos <bruno.lemos@live.com>
2024-12-05 21:08:53 -0300

    Modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,24 @@
 'use client'
 
-import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 
 
 export default function SetupPage() {
+  const onOpen = useStoreModal(state => state.onOpen)
+  const isOpen = useStoreModal(state => state.isOpen)
+  const onClose = useStoreModal(state => state.onClose)
+
+  useEffect(() => {
+    if(!isOpen){
+      onOpen()
+    }
+  }, [isOpen, onOpen, onClose])
+
   return (
     <div className="p-4">
-      <Modal title="Teste" description="Teste" isOpen onClose={() => {}}>
-        Children
-      </Modal>
+     
     </div>
   );
 }
