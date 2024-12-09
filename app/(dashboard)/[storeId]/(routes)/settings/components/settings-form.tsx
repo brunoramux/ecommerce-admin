7e1e186e86ddb4d57aac5d67c066fafc087eca6d
Bruno Ramos Lemos <bruno.lemos@live.com>
2024-12-09 20:44:26 -0300

    Delete store

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,21 +1,105 @@
 'use client'
 
 import { Heading } from "@/components/heading"
+import { AlertModal } from "@/components/modals/alert-modal"
 import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
+import { zodResolver } from "@hookform/resolvers/zod"
 import type { Store } from "@prisma/client"
+import axios from "axios"
 import { Trash } from "lucide-react"
+import { useParams, useRouter } from "next/navigation"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import toast from "react-hot-toast"
+import { z } from "zod"
 
 interface SettingsFormProps {
   initialData: Store
 }
 
-export default function SettingsForm({ initialData }: SettingsFormProps){
+const settingsFormSchema = z.object({
+  name: z.string().min(1, {message: "Value required"})
+})
+
+type SettingsFormValues = z.infer<typeof settingsFormSchema>
+
+
+export const SettingsForm: React.FC<SettingsFormProps> = ({
+  initialData
+}) => {
+  const params = useParams()
+  const router = useRouter()
+  const [open, setOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(settingsFormSchema),
+    defaultValues: initialData
+  })
+
+  const onSubmit = async (data: SettingsFormValues) => {
+    try {
+      setLoading(true)
+      await axios.patch(`/api/stores/${params.storeId}`, data)
+      router.refresh()
+      setLoading(false)
+      toast.success("Store updated.")
+    } catch (error) {
+      toast.error("Something went wrong.")
+      setLoading(false)
+    }
+  }
+
+  const onDelete = async () => {
+    try {
+      setLoading(true)
+      await axios.delete(`/api/stores/${params.storeId}`)
+      router.refresh()
+      router.push("/")
+      toast.success("Store deleted.")
+    } catch (error) {
+      toast.error("Make sure you removed all products and categorics first.")
+    } finally {
+      setLoading(false)
+      setOpen(false)
+    }
+  }
+
   return (
-    <div className="flex items-center justify-between">
+    <>
+      <AlertModal isOpen={open} onClose={() => setOpen(false)} loading={loading} onConfirm={() => onDelete()}/>
+      <div className="flex items-center justify-between">
        <Heading title="Settings" description="Manage store preferences"/>
-       <Button variant="destructive" size="icon" onClick={() => {}}> 
+       <Button variant="destructive" disabled={loading} size="icon" onClick={() => setOpen(true)}> 
         <Trash className="h4- w-4"/>
        </Button>
-    </div>
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+          <div className="grid grid-cols-3 gap-8">
+            <FormField 
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input disabled={loading} placeholder="Store name" {...field}/>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div> 
+          <Button disabled={loading}>
+            Save changes
+          </Button>
+        </form>
+      </Form> 
+    </>
   )
 }
--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,7 +1,7 @@
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import SettingsForm from "./components/settings-form";
+import { SettingsForm } from "./components/settings-form";
 
 interface SettingsPageParams {
   params: {
